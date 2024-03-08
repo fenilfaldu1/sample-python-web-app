06a6a0a798aea34239301d45b68ad6e3492a42ae
fenilfaldu1 <155763749+fenilfaldu1@users.noreply.github.com>
2024-03-08 14:02:48 -0500

    Update pipenv.sh

--- scripts/pipenv.sh
+++ scripts/pipenv.sh
@@ -2,6 +2,14 @@
 
 chown nginx:nginx -R /var/www/SampleApp/
 cd /var/www/SampleApp
+# Install pyenv (assuming it's not already installed)
+curl https://pyenv.run | bash
+
+# Add pyenv to the PATH
+export PATH="$HOME/.pyenv/bin:$PATH"
+
+# Initialize pyenv
+eval "$(pyenv init --path)"
 pyenv install 3.11.0
 pipenv --python $(pyenv root)/versions/3.11.0/bin/python3.11
 pipenv install
