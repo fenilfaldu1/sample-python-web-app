f27c31de0eed1a6d8e57b62aae8a37eefbbe2736
fenilfaldu1 <155763749+fenilfaldu1@users.noreply.github.com>
2024-03-08 14:47:13 -0500

    Update pipenv.sh

--- scripts/pipenv.sh
+++ scripts/pipenv.sh
@@ -1,5 +1,28 @@
 #!/bin/bash -xe
 
+# Remove existing pyenv directory
+rm -rf /root/.pyenv
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Add pyenv to the PATH
+export PATH="$HOME/.pyenv/bin:$PATH"
+
+# Initialize pyenv
+eval "$(pyenv init --path)"
+
+# Install Python 3.11 using pyenv
+pyenv install 3.11.0
+
+# Change ownership of directory to nginx user
 chown nginx:nginx -R /var/www/SampleApp/
+
+# Change into the project directory
 cd /var/www/SampleApp
+
+# Specify Python 3.11 for the Pipenv environment
+pipenv --python $(pyenv root)/versions/3.11.0/bin/python3.11
+
+# Install project dependencies using Pipenv
 pipenv install
